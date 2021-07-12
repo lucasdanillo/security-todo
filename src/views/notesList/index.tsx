781b5c8fdf2bda93bf60368f59a0858595605e5d
lucasdanillo <lucasdanillo1998@gmail.com>
2021-07-12 20:30:50 -0300

    create modal todo

--- src/views/notesList/index.tsx
+++ src/views/notesList/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import TodoList from '../../components/todoList';
+import { Button, Modal, Form, Container } from 'react-bootstrap';
+
 
 interface Todo {
   id: string;
@@ -10,6 +12,23 @@ interface Todo {
 function NotesList() {
 
   const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [data, setData] = useState('');
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  function handleSaveToDo() {
+    //mandar p/ banco
+    console.log(titulo);
+    console.log(descricao);
+    console.log(data);
+    setShow(false);
+  }
 
   var staticTodoList = [
     {
@@ -34,7 +53,45 @@ function NotesList() {
   }, []);
 
   return (
-      <TodoList list={todoList}/>
+    <div>
+      <TodoList list={todoList} />
+      <Button variant="primary" onClick={handleShow}>
+        Nova Nota
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cadastro de Nota</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Form style={{
+              width: '45%'
+            }}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Título da Nota</Form.Label>
+                <Form.Control type="text" placeholder="Título" onChange={(e) => setTitulo(e.target.value)} />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Descrição</Form.Label>
+                <Form.Control type="text" placeholder="Descrição" onChange={(e) => setDescricao(e.target.value)} />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Data</Form.Label>
+                <Form.Control type="text" placeholder="Data" onChange={(e) => setData(e.target.value)} />
+              </Form.Group>
+            </Form>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+          <Button variant="primary" onClick={handleSaveToDo}>
+            Salvar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
 
